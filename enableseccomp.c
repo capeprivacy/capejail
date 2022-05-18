8703a38d42bd1168dc17f9086ed5c783e4e8aa7c
Kyle Kloberdanz <kyle@capeprivacy.com>
2022-05-18 13:42:17 -0500

    display particular syscall rule number that fails

--- enableseccomp.c
+++ enableseccomp.c
@@ -5,8 +5,12 @@
 #include "enableseccomp.h"
 
 #define TRY_RULE(A) do { \
-    if ((A)) { \
-        fprintf(stderr, "failed to add seccomp rule"); \
+    if (seccomp_rule_add(ctx, SCMP_ACT_ALLOW, (A), 0) != 0) { \
+        fprintf( \
+            stderr, \
+            "failed to add seccomp rule for syscall number: %d\n", \
+            (A) \
+        ); \
         goto fail; \
     } \
 } while (0)
@@ -20,17 +24,17 @@ int enable_seccomp() {
         goto fail;
     }
 
-    TRY_RULE(seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(rt_sigreturn), 0));
-    TRY_RULE(seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(exit), 0));
-    TRY_RULE(seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(exit_group), 0));
-    TRY_RULE(seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(read), 0));
-    TRY_RULE(seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(write), 0));
-    TRY_RULE(seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(getrandom), 0));
-    TRY_RULE(seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(close), 0));
-    TRY_RULE(seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(rt_sigaction), 0));
-    TRY_RULE(seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(munmap), 0));
-    TRY_RULE(seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(mmap), 0));
-    TRY_RULE(seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(brk), 0));
+    TRY_RULE(SCMP_SYS(rt_sigreturn));
+    TRY_RULE(SCMP_SYS(exit));
+    TRY_RULE(SCMP_SYS(exit_group));
+    TRY_RULE(SCMP_SYS(read));
+    TRY_RULE(SCMP_SYS(write));
+    TRY_RULE(SCMP_SYS(getrandom));
+    TRY_RULE(SCMP_SYS(close));
+    TRY_RULE(SCMP_SYS(rt_sigaction));
+    TRY_RULE(SCMP_SYS(munmap));
+    TRY_RULE(SCMP_SYS(mmap));
+    TRY_RULE(SCMP_SYS(brk));
 
     return seccomp_load(ctx);
 
