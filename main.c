49d7db463d2d55fca0e83a9da6e978f600c41633
Kyle Kloberdanz <kyle@capeprivacy.com>
2022-06-03 13:54:57 -0500

    program name needs -- before it

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ static void print_usage() {
         stderr,
         "%s: enable a secure compute environment in a jail that blocks certain syscalls\n"
         "usage:\n"
-        "\t%s -u USER -r CHROOT [-d DIRECTORY] PROGRAM -- [ARGS]\n\n"
+        "\t%s -u USER -r CHROOT [-d DIRECTORY] -- PROGRAM [ARGS]\n\n"
         "\t-d\tdirectory to start in within jail\n\n"
         "\t-r\tpath to chroot directory to use in jail\n\n"
         "\t-u\tuser to run as within the jail\n\n"
