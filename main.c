f2d514272c573d43697ac91d0925a7fd7e294886
Kyle Kloberdanz <kyle@capeprivacy.com>
2022-06-22 13:44:00 -0500

    clang-format-14 finally supports my prefered style guide

--- main.c
+++ main.c
@@ -14,17 +14,20 @@
 static char *program_name = NULL;
 
 static void print_usage(void) {
-    fprintf(stderr,
-            "%s: enable a secure compute environment in a jail that blocks "
-            "certain syscalls\n"
-            "usage:\n"
-            "\t%s [OPTION] -- PROGRAM [ARGS]\n\n"
-            "\t-d\tdirectory to start in within jail\n\n"
-            "\t-r\tpath to chroot directory to use in jail\n\n"
-            "\t-u\tuser to run as within the jail\n\n"
-            "\t-I\tinsecure mode, launch with seccomp disabled\n\n"
-            "NOTE: should be run as root or with sudo to allow chroot\n\n",
-            program_name, program_name);
+    fprintf(
+        stderr,
+        "%s: enable a secure compute environment in a jail that blocks "
+        "certain syscalls\n"
+        "usage:\n"
+        "\t%s [OPTION] -- PROGRAM [ARGS]\n\n"
+        "\t-d\tdirectory to start in within jail\n\n"
+        "\t-r\tpath to chroot directory to use in jail\n\n"
+        "\t-u\tuser to run as within the jail\n\n"
+        "\t-I\tinsecure mode, launch with seccomp disabled\n\n"
+        "NOTE: should be run as root or with sudo to allow chroot\n\n",
+        program_name,
+        program_name
+    );
 }
 
 static void logerror(const char *fmt, ...) {
@@ -52,8 +55,14 @@ static void logerror(const char *fmt, ...) {
  * On success: returns the index in argv of the program and arguments to exec
  *             in the jail
  */
-static int parse_opts(int argc, char **argv, char **root, char **user,
-                      const char **directory, bool *insecure_mode) {
+static int parse_opts(
+    int argc,
+    char **argv,
+    char **root,
+    char **user,
+    const char **directory,
+    bool *insecure_mode
+) {
     int c;
     if (!root || !user || !directory || !insecure_mode) {
         logerror("parse_opts got null pointer for root and/or user and/or "
@@ -128,7 +137,8 @@ int main(int argc, char **argv) {
             logerror(
                 "could not chroot to: '%s' (are you root? does the directory "
                 "exist?)",
-                root);
+                root
+            );
             exit(EXIT_FAILURE);
         }
     }
